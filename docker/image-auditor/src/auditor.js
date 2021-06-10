dbbffe059e248f8afe94a9676b6f9e565853bc92
Eric Broutba <eric.bousbaa@hotmail.com>
2021-06-10 10:15:07 +0200

    auditeur

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,6 +1,15 @@
+const dgram = require("dgram");
 
+const PORT = 41234
+const IP   = "127.0.0.1"
 
-
-
-// TODO
 console.log("Audtior...")
+
+const socket = dgram.createSocket("udp4")
+
+
+socket.on("message", (message) => {
+   let msg = JSON.parse(message);
+   console.log("MESSAGE");
+   console.log(msg);
+});
