6c32a965e4c8c1f10d3e412cf1b145d7fbf2a95b
Ilias <igoujgali@gmail.com>
2021-06-10 12:37:32 +0200

    add prettier config file and format code

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -5,54 +5,54 @@ const PORT_UDP = 1450;
 const PORT_TCP = 2205;
 
 const INSTRUMENTS = {
-  "ti-ta-ti": "piano",
-  pouet: "trumpet",
-  trulu: "flute",
-  "gzi-gzi": "violin",
-  "boum-boum": "drum",
+    "ti-ta-ti": "piano",
+    pouet: "trumpet",
+    trulu: "flute",
+    "gzi-gzi": "violin",
+    "boum-boum": "drum",
 };
 
 const socket = dgram.createSocket("udp4");
 let musicians = new Map();
 
 socket.bind(PORT_UDP, function () {
-  console.log("Joining multicast group");
-  socket.addMembership("239.255.22.5");
+    console.log("Joining multicast group");
+    socket.addMembership("239.255.22.5");
 });
 
 socket.on("message", (message) => {
-  let msg = JSON.parse(message);
-  musicians.set(msg.uuid, {
-    instrument: INSTRUMENTS[msg.sound],
-    lastMsg: new Date(),
-  });
+    let msg = JSON.parse(message);
+    musicians.set(msg.uuid, {
+        instrument: INSTRUMENTS[msg.sound],
+        lastMsg: new Date(),
+    });
 });
 
 const server = net.createServer();
 server.listen(PORT_TCP);
 server.on("connection", (socket) => {
-  const aryMusicians = [];
-  musicians.forEach((v, k) => {
-    aryMusicians.push({
-      uuid: k,
-      instrument: v.instrument,
-      activeSince: v.lastMsg,
+    const aryMusicians = [];
+    musicians.forEach((v, k) => {
+        aryMusicians.push({
+            uuid: k,
+            instrument: v.instrument,
+            activeSince: v.lastMsg,
+        });
     });
-  });
-  socket.write(JSON.stringify(aryMusicians));
-  socket.end();
+    socket.write(JSON.stringify(aryMusicians));
+    socket.end();
 });
 server.on("data", (socket) => {
-  console.log(socket);
+    console.log(socket);
 });
 server.on("error", (err) => {
-  throw err;
+    throw err;
 });
 
 setInterval(() => {
-  musicians = new Map(
-    [...musicians].filter(([k, v]) => {
-      return Date.now() - v.lastMsg <= 5000;
-    })
-  );
+    musicians = new Map(
+        [...musicians].filter(([k, v]) => {
+            return Date.now() - v.lastMsg <= 5000;
+        })
+    );
 }, 1000);
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -2,46 +2,45 @@ import { v4 as uuid } from "uuid";
 import dgram from "dgram";
 
 const instruments = {
-  piano: "ti-ta-ti",
-  trumpet: "pouet",
-  flute: "trulu",
-  violin: "gzi-gzi",
-  drum: "boum-boum",
+    piano: "ti-ta-ti",
+    trumpet: "pouet",
+    flute: "trulu",
+    violin: "gzi-gzi",
+    drum: "boum-boum",
 };
 
 class Musician {
-  constructor(instrument) {
-    if (!Object.keys(instruments).includes(instrument)) {
-      throw new Error("L'instrument n'existe pas");
+    constructor(instrument) {
+        if (!Object.keys(instruments).includes(instrument)) {
+            throw new Error("L'instrument n'existe pas");
+        }
+        this.instrument = { name: instrument, sound: instruments[instrument] };
+        this.id = uuid();
     }
-    this.instrument = { name: instrument, sound: instruments[instrument] };
-    this.id = uuid();
-  }
 
-  play() {
-    const data = {
-      uuid: this.id,
-      sound: this.instrument.sound,
-    };
-    console.log(data);
-    return data;
-  }
+    play() {
+        const data = {
+            uuid: this.id,
+            sound: this.instrument.sound,
+        };
+        console.log(data);
+        return data;
+    }
 }
-
 
 const pianist = new Musician(process.argv[2]);
 const socket = dgram.createSocket("udp4");
 setInterval(() => {
-  socket.send(
-    JSON.stringify(pianist.play()),
-    1450,
-    "239.255.22.5",
-    (err, bytes) => {
-      if (err !== null) {
-        console.error("ERR:", err);
-        socket.close();
-        clearInterval(this);
-      }
-    }
-  );
+    socket.send(
+        JSON.stringify(pianist.play()),
+        1450,
+        "239.255.22.5",
+        (err, bytes) => {
+            if (err !== null) {
+                console.error("ERR:", err);
+                socket.close();
+                clearInterval(this);
+            }
+        }
+    );
 }, 1000);
